79d7859df928b69491afb1495e98a5ec575bfb9c
theSlyest <theslyest@hotmail.com>
2022-08-17 03:53:58 +0000

    Debugged ToolSet class

--- lib/utils/tool_set.dart
+++ lib/utils/tool_set.dart
@@ -8,13 +8,16 @@ class ToolSet {
 
     if (len == 9) {
       return Color.fromARGB(
-          int.parse(str.substring(1, 3)),
-          int.parse(str.substring(3, 5)),
-          int.parse(str.substring(5, 7)),
-          int.parse(str.substring(7)));
+          int.parse(str.substring(1, 3), radix: 16),
+          int.parse(str.substring(3, 5), radix: 16),
+          int.parse(str.substring(5, 7), radix: 16),
+          int.parse(str.substring(7), radix: 16));
     } else {
-      return Color.fromARGB(255, int.parse(str.substring(1, 3)),
-          int.parse(str.substring(3, 5)), int.parse(str.substring(5)));
+      return Color.fromARGB(
+          255,
+          int.parse(str.substring(1, 3), radix: 16),
+          int.parse(str.substring(3, 5), radix: 16),
+          int.parse(str.substring(5), radix: 16));
     }
   }
 
@@ -22,22 +25,21 @@ class ToolSet {
     return Color(0xFF000000 + rgb);
   }
 
-  int colorToInt(Color color) {
-    return (color.red << 16) + (color.green << 8) + color.blue;
-  }
+  static int colorToInt(Color color) =>
+      (color.red << 16) + (color.green << 8) + color.blue;
 
-  Rect intersection(final Rect rect1, final Rect rect2) {
-    if (rect1.size.width == 0 ||
-        rect1.size.height == 0 ||
-        rect2.size.width == 0 ||
-        rect2.size.height == 0) {
+  static Rect intersection(final Rect rect1, final Rect rect2) {
+    if (rect1.size.width == 0.0 ||
+        rect1.size.height == 0.0 ||
+        rect2.size.width == 0.0 ||
+        rect2.size.height == 0.0) {
       return Rect.zero;
     }
 
     double left = rect1.left > rect2.left ? rect1.left : rect2.left;
     double right = rect1.right < rect2.right ? rect1.right : rect2.right;
-    double top = rect1.top < rect2.top ? rect1.top : rect2.top;
-    double bottom = rect1.bottom > rect2.bottom ? rect1.bottom : rect2.bottom;
+    double top = rect1.top > rect2.top ? rect1.top : rect2.top;
+    double bottom = rect1.bottom < rect2.bottom ? rect1.bottom : rect2.bottom;
 
     if (left > right || top > bottom) {
       return Rect.zero;
@@ -46,10 +48,7 @@ class ToolSet {
     }
   }
 
-  int findInStringArray(final List<String> arr, final String str) =>
-      arr.indexOf(str);
-
-  bool isFileExist(String fileName) {
+  static bool isFileExist(String fileName) {
     bool res = io.File(fileName).existsSync();
     // TODO Add popup notification logic (using Flame sdk)
     return res;
