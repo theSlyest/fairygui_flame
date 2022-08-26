50d8d0027872c1b6e17257bb6570861fdcddcb1b
theSlyest <theslyest@hotmail.com>
2022-08-26 16:27:58 +0000

    Added PixelHitTestData class

---
+++ lib/event/pixel_hit_test_data.dart
@@ -0,0 +1,11 @@
+class PixelHitTestData {
+  int pixelWidth;
+  double scale;
+  List<int>? pixels;
+
+  PixelHitTestData()
+      : pixelWidth = 0,
+        scale = 1;
+
+  void load(String buffer) {}
+}
